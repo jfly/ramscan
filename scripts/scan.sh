7e1c0a2e3f416f78401bd73705792ed1eae3bac6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2021-01-20 22:43:32 -0800

    Real scanning + progress bar.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -9,15 +9,26 @@ __output_file="$1"
 
 __tmp_file=/tmp/scan.tmp
 
-echo "0%"
-sleep 1
-echo "10%"
 
-wget -O "$__tmp_file" -q https://picsum.photos/1200/600
+function fakeit() {
+    printf "Progress: 0%%"
+    sleep 1
+    printf "\rProgress: 10%%"
 
-echo "50%"
-sleep 1
-echo "60%"
-sleep 1
-echo "100%"
+    wget -O "$__tmp_file" -q https://picsum.photos/1200/600
+
+    printf "\rProgress: 50%%"
+    sleep 1
+    printf "\rProgress: 60%%"
+    sleep 1
+    printf "\rProgress: 100%%"
+}
+
+function scanit() {
+    DEVICE=pixma:04A91913_4B6895
+    scanimage --device "$DEVICE" --format=jpeg --output-file "$__tmp_file" --resolution 300 --progress
+}
+
+# fakeit
+scanit
 mv "$__tmp_file" "$__output_file"
--- server/methods.ts
+++ server/methods.ts
@@ -9,32 +9,41 @@ function getScriptsDir() {
     return path.join(getProjectRootDir(), "scripts");
 }
 
+const PROGRESS_RE = /Progress: (?<progress>[0-9]+(.[0-9]+)?)%/;
+
 function scanFile(scanId: string, absoluteFilePath: string): Promise<void> {
     const scanScript = path.join(getScriptsDir(), "scan.sh");
 
     const scan = spawn(scanScript, [absoluteFilePath]);
 
     return new Promise((resolve, reject) => {
-        scan.stdout.on(
-            "data",
-            Meteor.bindEnvironment((data: Buffer) => {
-                const dataStr = data.toString("utf8").trim();
-                if (dataStr.endsWith("%")) {
-                    const progress = parseInt(
-                        dataStr.substring(0, dataStr.length - 1)
+        const onData = function (prefix: string) {
+            return Meteor.bindEnvironment(function (data: Buffer) {
+                // The incoming data might be something like: "Progress:
+                // 5%\rProgress: 6%". If so, just ignore all but the last
+                // progress update.
+                const dataStrs = data
+                    .toString("utf8")
+                    .split("\r")
+                    .filter((line) => line.length > 0);
+                const dataStr = dataStrs[dataStrs.length - 1];
+                const match = PROGRESS_RE.exec(dataStr);
+                if (match && match.groups) {
+                    const progress = parseFloat(
+                        match.groups.progress.substring(0, dataStr.length - 1)
                     );
                     ScansCollection.update(
                         { _id: scanId },
                         { $set: { progress } }
                     );
+                } else {
+                    console.log(`${prefix}: ${JSON.stringify(dataStrs)}`);
                 }
-                console.log(`stdout: ${dataStr}`);
-            })
-        );
+            });
+        };
 
-        scan.stderr.on("data", (data: Buffer) => {
-            console.error(`stderr: ${data}`);
-        });
+        scan.stdout.on("data", onData("stdout"));
+        scan.stderr.on("data", onData("stderr"));
 
         scan.on("close", (code) => {
             console.log(`child process exited with code ${code}`);
